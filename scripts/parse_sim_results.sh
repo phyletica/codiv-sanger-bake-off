f1a1a165c6b4d704412af6c4f079e7902340eac3
Jamie R. Oaks <joaks1@gmail.com>
2020-10-04 21:14:02 -0500

    Adding scripts for parsing sim results.
    Adding "parse_sim_results.py" Python script for parsing the results of
    ecoevolity from running on datasets simulated by simcoevolity.  The
    results are tabulated in TSV files.
    
    Adding "parse_sim_results.sh" shell script simply runs the python script
    after activating the project's Python environment.

---
+++ scripts/parse_sim_results.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    if [ -f "${PBS_O_HOME}/.bashrc" ]
+    then
+        source "${PBS_O_HOME}/.bashrc"
+    fi
+    cd $PBS_O_WORKDIR
+fi
+
+if [ ! -f "${project_dir}/pyenv/bin/activate" ]
+then
+    echo "ERROR: Python environment \"${project_dir}/pyenv\" does not exist."
+    echo "       You probably need to run the project setup script."
+    exit 1
+fi
+source "${project_dir}/pyenv/bin/activate"
+
+
+./parse_sim_results.py $@
