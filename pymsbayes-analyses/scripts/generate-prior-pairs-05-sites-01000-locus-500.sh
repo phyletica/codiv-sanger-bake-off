d950f6ca7f6eb91fa3c16cdf6e3571c6edb8d9b7
Jamie R. Oaks <joaks1@gmail.com>
2020-10-07 16:01:09 -0500

    Adding configs and scripts for pymsbayes analyses.

---
+++ pymsbayes-analyses/scripts/generate-prior-pairs-05-sites-01000-locus-500.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+fi
+
+nsites="01000"
+npairs="05"
+locus_length="500"
+
+nprocs=8
+nprior=500000
+batch_size=6250
+nsums=100000
+seed=1384268
+
+cfg_name="pairs-${npairs}-sites-${nsites}-locus-${locus_length}"
+
+output_dir="../priors/${cfg_name}"
+if [ ! -d "$output_dir" ]
+then
+    mkdir -p $output_dir
+fi
+
+dmc.py --np $nprocs -r 1 -o "../configs/${cfg_name}.cfg" -p "../configs/${cfg_name}.cfg" -n $nprior --num-posterior-samples $batch_size --prior-batch-size $batch_size --num-standardizing-samples $nsums --output-dir "$output_dir" --seed $seed --generate-samples-only 1>generate-prior-${cfg_name}.sh.out 2>&1
---
+++ pymsbayes-analyses/scripts/run-reject-fixed-independent-pairs-05-sites-10000-locus-500.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+fi
+
+nsites="10000"
+npairs="05"
+locus_length="500"
+
+reps=1000
+nprocs=8
+nprior=500000
+batch_size=12500
+nsums=100000
+npost=2000
+nquantiles=1000
+sortindex=0
+seed=37851841
+
+prior_name="pairs-${npairs}-sites-${nsites}-locus-${locus_length}"
+cfg_name="fixed-independent-${prior_name}"
+
+output_dir="../results/${cfg_name}"
+if [ ! -d "$output_dir" ]
+then
+    mkdir -p $output_dir
+fi
+
+dmc.py --np $nprocs -r $reps -o "../configs/${cfg_name}.cfg" -p ../priors/${prior_name}/pymsbayes-results/pymsbayes-output/prior-stats-summaries -n $nprior --prior-batch-size $batch_size --num-posterior-samples $npost --num-standardizing-samples $nsums -q $nquantiles --sort-index $sortindex --output-dir $output_dir --seed $seed --no-global-estimate --compress 1>run-reject-${cfg_name}.sh.out 2>&1
