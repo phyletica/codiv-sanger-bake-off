be5a0b1fe5313c009ecb2be2de1eda38122500eb
Tanner C. Myers <tannercmyers@gmail.com>
2022-01-18 11:04:50 -0600

    Updating variables and providing clearer instructions to run script.
    I needed to make it clear that the directory variable assigned as a command line argument
    should be an absolute path. I also changed some of the variable names.

--- scripts/dryadDownload.sh
+++ scripts/dryadDownload.sh
@@ -1,23 +1,29 @@
 #! /usr/bin/env bash
 
 # Steps this script will do:
-## 1. Given a list of dois, download from Dryad
-DRYAD=$1
-TEMP_DIR=$2
 
-## 2. unzip downloaded Dryad repo
+## 1. Given a list of dois, download from Dryad
+
+	# Supply list of DOIs associated with the empirical studies with data we will be analyzing
+	DRYAD_LIST=$1
+	
+	# Supply full path to directory where data is to be deposited: PATH-TO-YOUR-REPO-COPY/codiv-sanger-bake-off/data/empirical-datasets	
+	DATA_DIR=$2
+
+## 2. Create directories for those datasets, download the Dryad repos associated with each project, and unzip downloaded files
 
 while read item 
 	do 
-		author=`echo "$item" | awk -v FS="\t" '{ print $1}'`
+		AUTHOR=`echo "$item" | awk -v FS="\t" '{ print $1}'`
 	 
-		#echo $author
-		mkdir -p $TEMP_DIR/$author
-		link=`echo "$item" | awk -v FS="\t" '{print $3}'`
-		#echo $link
+		# Name subdirectory after author key
+		mkdir -p $TEMP_DIR/$AUTHOR
+
+		# Assign the DOI link as a variable
+		LINK=`echo "$item" | awk -v FS="\t" '{print $3}'`
 		
-		cd $TEMP_DIR/$author
-        	wget $link
+		cd $DATA_DIR/$AUTHOR
+        	wget $LINK
 		
 		for file in `ls`
 			do
