de65d115ccf3ef0feb2f624bed7f673855599b49
Jamie R. Oaks <joaks1@gmail.com>
2024-09-25 15:17:05 -0500

    Adding ecoevolity scripts for empirical data.
    Adding Bash scripts for running ecoevolity on the empirical
    Philippines and Mediterranean data.

---
+++ scripts/empirical-ecoevolity-analyses/mediterranean-all-pairs-run-2.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+set -e
+
+run=2
+config_prefix="mediterranean-all-pairs"
+
+# Get path to directory of this script
+script_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+project_dir="$( dirname "$( dirname "$script_dir" )" )"
+
+# Load modules
+echo "Loading modules..."
+source "${project_dir}/modules-to-load.sh" >/dev/null 2>&1 || echo "    No modules loaded"
+
+emp_output_dir="${project_dir}/empirical-ecoevolity-output"
+if [ ! -d "$emp_output_dir" ]
+then
+    mkdir "$emp_output_dir"
+fi
+
+output_dir="${emp_output_dir}/${config_prefix}"
+if [ ! -d "$output_dir" ]
+then
+    mkdir "$output_dir"
+fi
+
+config_path="${project_dir}/data/empirical/ecoevolity-format/Papadopoulou-Knowles-2015/configs/${config_prefix}-config.yml"
+prefix="${output_dir}/run-${run}-"
+
+"${project_dir}/bin/ecoevolity" --seed "$run" --prefix "$prefix" "$config_path" 1>"${prefix}${config_prefix}.out" 2>&1
